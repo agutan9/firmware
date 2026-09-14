66068baaa40a92f99b720152028559cad5e14281
Tim Brussaard <tbrussaard@tudelft.nl>
2026-09-14 17:59:23 +0200

    Can upload from both arduino and platformIO

--- src/Qbead_MDP.ino
+++ src/Qbead_MDP.ino
@@ -1,46 +1,33 @@
 #include <Arduino.h>
-#include <Qbead.h>
-#include "LSM6DS3.h"
-#include "Wire.h"
+#include <Adafruit_NeoPixel.h>
+#include <LSM6DS3.h>
+#include <math.h>
 
-LSM6DS3 myIMU(I2C_MODE, 0x6A);
-int64_t initResult = -1;
-uint8_t whoAmI = -1;
+#include <bluefruit.h>
+#include <Qbead.h>
+
+Qbead::Qbead bead;
+
+// put function declarations here:
+//int myFunction(int, int);
 
 void setup() {
-  Serial.begin(9600);
-  delay(7000);
-
-  Wire.begin();
-
-  #ifdef PIN_LSM6DS3TR_C_INT1
-    Serial.print("INT1 pin number: ");
-    Serial.println(PIN_LSM6DS3TR_C_INT1);
-    pinMode(PIN_LSM6DS3TR_C_INT1, INPUT);
-    Serial.print("INT1 idle level: ");
-    Serial.println(digitalRead(PIN_LSM6DS3TR_C_INT1));
-  #else
-    Serial.println("PIN_LSM6DS3TR_C_INT1 not defined either!");
-  #endif
-
-  whoAmI = 0;
-  myIMU.readRegister(&whoAmI, 0x0F);
-  Serial.print("WHO_AM_I raw value: 0x");
-  Serial.println(whoAmI, HEX);
-
-  initResult = myIMU.begin();
+  delay(20);
+  bead.begin();
+  bead.setBrightness(25);
+  bead.testPixels();
+  delay(100);
 }
 
 void loop() {
-  Serial.print("begin(): ");
-  Serial.print((long)initResult);
-  Serial.print("whoAmI(): ");
-  Serial.print((long)whoAmI);
-  Serial.print("  Acc X: ");
-  Serial.print(myIMU.readFloatAccelX());
-  Serial.print("  Y: ");
-  Serial.print(myIMU.readFloatAccelY());
-  Serial.print("  Z: ");
-  Serial.println(myIMU.readFloatAccelZ());
-  delay(1000);
+  bead.readIMU(false);
+  bead.clear();
+  bead.setBloch_deg_smooth(bead.t_ble, bead.p_ble, bead.c_ble);
+  bead.show();
+  delay(20);
 }
+
+// put function definitions here:
+//int myFunction(int x, int y) {
+//  return x + y;
+//}
